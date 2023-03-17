1c6adacb780faf1492db72df1dccd9181f9ebf92
Ysamwel <yvonneatieno2000@gmail.com>
2023-03-16 20:16:43 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - print quote
+ *
+ * Description: print Dora's quote
+ *
+ * Return: 1 (success)
+ */
+
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+
+}
