23b399e76f9205c5c87972eee97e3c99b725ee43
Ysamwel <yvonneatieno2000@gmail.com>
2023-03-28 00:16:35 +0000

    prints a string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - print a string in reverse
+ * @s: is the char
+ */
+
+
+void print_rev(char *s)
+{
+	int a, b;
+
+	b = 0;
+	while (s[b] != '\0')
+		n++;
+
+	for (a = b - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
