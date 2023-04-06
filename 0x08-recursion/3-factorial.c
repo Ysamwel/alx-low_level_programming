68b55cd61fdc57817f19b84d9f6d3c8f668d659c
Ysamwel <yvonneatieno2000@gmail.com>
2023-04-06 23:29:25 +0000

    returns the factorial of a given number

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,27 @@
+i#include "main.h"
+/**
+ * factorial - product of all positive integers less than or equal to @n
+ * positive integer
+ *
+ * @n: integer given
+ * Return: integer
+ */
+
+int factorial(int n)
+{
+	int i;
+
+	if (n == 0)
+	{
+		return (1);
+	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		i = n * factorial(n - 1);
+	}
+	return (i);
+}
