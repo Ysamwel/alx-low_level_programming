9c4c0139da8317d728bb4ee4755560bfd6d4e717
Ysamwel <yvonneatieno2000@gmail.com>
2023-04-13 19:44:56 +0000

    allocating memory using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * malloc_checked - cause normal process termination with a status
+ * value of 98 if malloc fails
+ * @b: the memory
+ * Return: pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *i;
+
+	i = malloc(b);
+	if (i == NULL)
+		exit (98);
+	return (i);
+}
+
