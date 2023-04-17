fff6a8a0b18ce0ffd0267e9b1004a9a26bff9fcd
Ysamwel <yvonneatieno2000@gmail.com>
2023-04-17 23:02:01 +0000

    new type

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+lets hanble structures and typedef
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - new type
+ * @name: Name of the dog
+ * @age: age of the dog
+ * @owner: the owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+
+#endif
