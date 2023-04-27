8288bf6beefe5cd494c0f222780703d7398fac64
Ysamwel <yvonneatieno2000@gmail.com>
2023-04-27 22:02:05 +0000

    adds a new node at the beginning

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -19,7 +19,7 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-int _strlen(const char *s)
+int _strlen(const char *s);
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
