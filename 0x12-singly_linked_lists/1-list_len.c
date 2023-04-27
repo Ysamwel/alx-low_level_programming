b4feafce8410338b2f39d887b4ca5e895656d4f1
Ysamwel <yvonneatieno2000@gmail.com>
2023-04-27 21:09:39 +0000

    prints all the elements

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * list_len -  returns the number of elements in a linked list_t list
+ * @h: link
+ * Return: nodes
+ */
+
+size_t list_len(const list_t *h)
+{
+	int count = 0;
+
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
