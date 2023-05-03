cea7618fb04ba1491d8e1e0b44a63fb395136123
Ysamwel <yvonneatieno2000@gmail.com>
2023-05-03 01:48:49 +0000

    inserts a new node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,7 +10,7 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *current, *new = *head;
+	listint_t *new, *current = *head;
 	unsigned int i;
 
 	new = malloc(sizeof(listint_t));
